5e5edaadbf55e35b697e91478cbcc708f758ba56
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-06-23 12:34:10 +0600

    initai setup with intall tailwind css and daisy ui

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ED4E53",
+          secondary: "#1D2228",
+          accent: "#37CDBE",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
