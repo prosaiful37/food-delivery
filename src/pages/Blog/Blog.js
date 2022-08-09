8fc5d356227a097c12285e3342dd98c09ce9de0f
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-08-09 09:53:26 +0600

    blog page update

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,34 +1,35 @@
 import React from "react";
+import './Blog.css';
 
 const Blog = () => {
   return (
     <div class="hero min-h-screen bg-base-10">
       <div class="hero-content text-center">
-        <div class="max-w-md">
+        <div class="max-w-md shadow p-10">
           <h1 class="text-5xl font-bold">Comming Soon !! </h1>
           <p class="py-6 font-sans">Our Blog site under construction</p>
           <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
             <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
               <span class="countdown font-mono text-5xl">
-                <span style={{"--value":"15"}}></span>
+                <span className="days" ></span>
               </span>
               days
             </div>
             <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
               <span class="countdown font-mono text-5xl">
-                <span style={{"--value":"10"}}></span>
+                <span className="hours" ></span>
               </span>
               hours
             </div>
             <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
               <span class="countdown font-mono text-5xl">
-                <span style={{"--value": '24'}}></span>
+                <span className="minitue" ></span>
               </span>
               min
             </div>
             <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
               <span class="countdown font-mono text-5xl">
-                <span style={{"--value":"40"}}></span>
+                <span className="second" ></span>
               </span>
               sec
             </div>
