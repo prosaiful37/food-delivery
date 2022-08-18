7818eb3e1449163e4a15a40517cee3b05f21b1fd
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-08-18 11:25:12 +0600

    dashboard update

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,16 +1,21 @@
-import { ShoppingCartIcon, ThumbUpIcon, UserGroupIcon } from "@heroicons/react/solid";
+import {
+  ShoppingCartIcon,
+  ThumbUpIcon,
+  UserGroupIcon,
+} from "@heroicons/react/solid";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
 import auth from "../../firebase.init";
 import useAdmin from "../../Hooks/useAdmin";
+import logowhite from "../../Images/Logo/logo-light.png";
 // import { EmojiHappyIcon, EmojiSadIcon } from '@heroicons/react/solid'
 
 const Dashboard = () => {
   const [user] = useAuthState(auth);
 
   const [admin] = useAdmin(user);
-  
+
   return (
     <div class="drawer drawer-mobile">
       <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
@@ -18,7 +23,7 @@ const Dashboard = () => {
         <h2 className="md:text-4xl font-bold text-secondary">
           Welcome To our Dashboard{" "}
           <span className="text-primary text-2xl font-sans ">
-            <small> Mr.{user.displayName}  </small> 
+            <small> Mr.{user.displayName} </small>
           </span>{" "}
         </h2>
         <Outlet></Outlet>
@@ -26,18 +31,55 @@ const Dashboard = () => {
       <div class="drawer-side">
         <label for="dashboard-sidebar" class="drawer-overlay"></label>
         <ul class="menu p-4 overflow-y-auto w-80 bg-neutral text-white font-sans">
-          <li className="text-2xl mb-1 font-bold">
-            Slice pizza
+          <li className="">
+            <img className="w-auto" src={logowhite} alt="" />
           </li>
-          <hr />
+          <li className="mb-5">
+            <div class="card w-auto shadow">
+              <figure>
+                <div class="avatar mb-0">
+                  <div class="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <img src="https://placeimg.com/192/192/people" />
+                  </div>
+                </div>
+              </figure>
+              <div class="card-body text-center p-0">
+                <h2 class="card-title">{user.displayName}</h2>
+                <p>{user.email}</p>
+              </div>
+            </div>
+          </li>
           {/* <!-- Sidebar content here --> */}
           <li className="mb-1 mt-3">
-            <Link to="/dashboard"> <span> <ShoppingCartIcon className="h-5 w-auto ms-2"></ShoppingCartIcon> </span> Orders</Link>
+            <Link to="/dashboard">
+              {" "}
+              <span>
+                {" "}
+                <ShoppingCartIcon className="h-5 w-auto ms-2"></ShoppingCartIcon>{" "}
+              </span>{" "}
+              Orders
+            </Link>
           </li>
           <li className="mb-1">
-            <Link to="/dashboard/review"><span> <ThumbUpIcon className="h-5 w-auto ms-2"></ThumbUpIcon> </span> Reviews</Link>
+            <Link to="/dashboard/review">
+              <span>
+                {" "}
+                <ThumbUpIcon className="h-5 w-auto ms-2"></ThumbUpIcon>{" "}
+              </span>{" "}
+              Reviews
+            </Link>
           </li>
-          { admin && <li className="mb-1"><Link to="/dashboard/users"><span> <UserGroupIcon className="h-5 w-auto ms-2"></UserGroupIcon> </span> All Users</Link></li>}
+          {admin && (
+            <li className="mb-1">
+              <Link to="/dashboard/users">
+                <span>
+                  {" "}
+                  <UserGroupIcon className="h-5 w-auto ms-2"></UserGroupIcon>{" "}
+                </span>{" "}
+                All Users
+              </Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
--- src/pages/Menu/Menus.js
+++ src/pages/Menu/Menus.js
@@ -1,4 +1,4 @@
-import { ShoppingCartIcon } from "@heroicons/react/solid";
+import { PlusCircleIcon, ShoppingCartIcon } from "@heroicons/react/solid";
 import { useNavigate } from 'react-router-dom';
 import React from "react";
 import './Menu.css';
@@ -21,7 +21,7 @@ const Menus = ({ product }) => {
         <figure className="image-box">
           <img className="d-block" src={img} alt="foods" />
         </figure>
-        <div class="card-body">
+        <div class="card-body items-center">
           <h2 class="card-title">{name}</h2>
           <div class="rating">
             <input
@@ -53,13 +53,13 @@ const Menus = ({ product }) => {
           </div>
           <p className="text-left font-sans mb-3">{discription}</p>
           <p className="text-left font-sans font-bold my-3">
-            <span className="border p-2 rounded">${price}</span>
+            <span className="border p-2 rounded">Price - <span className="text-primary">${price}</span></span>
           </p>
           <hr />
           <div class="card-actions mt-5">
-            <button onClick={() => navigateToOrders(_id)} class="btn btn-primary font-sans font-bold text-white">
-              Order{" "}
-              <ShoppingCartIcon className="h-5 w-auto ms-2"></ShoppingCartIcon>
+            <button onClick={() => navigateToOrders(_id)} class="btn btn-outline btn-primary font-sans font-bold text-white">
+              Manage Order {" "} 
+              <PlusCircleIcon className="h-5 w-auto ml-3"></PlusCircleIcon>
             </button>
           </div>
         </div>
